9158a168c3e60e7dcbd8944d3c56ee5484796694
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-10-17 10:23:51 +0530

    add [About, Contact] Pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Test from "./components/Test"
 import  Head  from "./components/Head"
 import HeroNavBar from "./components/HeroNavBar"
 import HeroSection from "./pages/HeroSection"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
 import AdminLogin from "./pages/AdminLogin"
 import TeacherLogin from "./pages/TeacherLogin"
 import StudentLogin from "./pages/StudentLogin"
@@ -14,6 +16,8 @@ export default function App() {
     <>
     <Routes>
       <Route path="/" element={[<Head />, <HeroNavBar />, <HeroSection />]} />
+      <Route path="/about" element={[<Head />, <HeroNavBar />, <About />]} />
+      <Route path="/contact" element={[<Head />, <HeroNavBar />, <Contact />]} />
       <Route path="/admin/login" element={[<Head />, <HeroNavBar />, <AdminLogin />]} />
       <Route path="/teacher/login" element={[<Head />, <HeroNavBar />, <TeacherLogin />]} />
       <Route path="/student/login" element={[<Head />, <HeroNavBar />, <StudentLogin />]} />
