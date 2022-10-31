76742abc87c61bd78908a883058b1323cab24d53
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-10-31 13:46:38 +0530

    feat: login succesfull with accessing data from database

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -1,61 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { NavLink } from "react-router-dom";
+import { Route, Link, useNavigate } from "react-router-dom";
 
 
 
 export default function AdminLogin() {
-    const [auth, setAuth] = useState(false);
-    const [id, setId] = useState("");
+    const navigate = useNavigate();
 
     const [inputs, setInputs] = React.useState({});
+    const [authenticated, setAuthenticated] = useState(localStorage.getItem("authenticated"));
+    const [userId, setUserId] = useState(localStorage.getItem("userId"));
+    const [info, setInfo] = useState(localStorage.getItem("info"));
+
+    localStorage.setItem("authenticated", false);
+    localStorage.setItem("userId", null);
+    localStorage.setItem("info", null);
+    
+    // let auth = false;
 
     const handleChanges = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         setInputs(values => ({ ...values, [name]: value }));
-};
+    };
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(inputs);
         axios
             .post("http://localhost:9000/admin/login", inputs)
             .then((res) => {
-                console.log(res);
                 alert(res.data.message);
-                setAuth(res.data.auth);
-                console.log(auth);
-                setId(res.data.id);
-                console.log(id);
+                localStorage.setItem("authenticated", true);
+                localStorage.setItem("userId", res.data.user._id);
+                localStorage.setItem("info", JSON.stringify(res.data.user));
+                navigate("/admin/dashboard");
             })
             .catch((err) => {
-                console.log(err);
+                console.log('error : ',err);
+                alert(err.response.data.message);
             });
     };
 
     return (
         <>
-        {/* <form method="POST">
-            <h1>Admin login</h1>
-            <div>
-                <label htmlFor="username">Username : </label>
-                <input type="text" name="username" value={inputs.username || ""} onChange={handleChanges} placeholder="Username" required="" />
-            </div>
-            <div>
-                <label htmlFor="password">Password : </label>
-                <input type="password" name="password" value={inputs.password || ""} onChange={handleChanges} placeholder="Password" required="" />
-            </div>
-            <AdminDashboard id={id} />
-            
-            <button type="submit" onClick={handleSubmit}><Link to={auth ? "/admin/dashboard" : "/admin/login"}>Submit</Link></button>
-        </form> */}
-
         <p className="text-3xl p-10 font-semibold text-center">
             ADMIN LOGIN
         </p>
         <div className="p-6 m-auto bg-gray-100 rounded-md shadow-xl w-1/3">
-            <form>
+            <form onSubmit={handleSubmit}>
+                <div className="p-4">
+                    <div className="p-2">
+                        <label
+                        htmlFor="username"
+                        className="block text-xl">
+                            Username :
+                        </label>
+                        <div className="flex">
+                            <input
+                            type="text"
+                            name="username"
+                            onChange={handleChanges}
+                            className="block w-full px-4 py-2 mt-2 border"/>
+                        </div>
+                    </div>
+                    <div className="p-2">
+                        <label
+                        htmlFor="password"
+                        className="block text-xl">
+                            Password :
+                        </label>
+                        <div className="flex">
+                            <input
+                            onChange={handleChanges}
+                            type="password"
+                            name="password"
+                            className="block w-full px-4 py-2 mt-2 border"/>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="flex items-center justify-center">
+                    
+                        <button
+                        type="submit"
+                        className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-slate-700">
+                            {/* <Link to={auth && <Route to="/admin/dashboard" />}> */}
+                                LOGIN
+                            {/* </Link> */}
+                        </button>
+                </div>
+            </form>
+            {/* <form>
                 <div className="p-4">
                     <div className="p-2">
                         <label
@@ -92,7 +128,7 @@ export default function AdminLogin() {
                         </button>
                     </NavLink>
                 </div>
-            </form>
+            </form> */}
         </div>
         </>
     )
