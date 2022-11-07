fdc4e0cc31316245fd08cdfdd8caf31ee976994a
DrasticCoder <drasticoder@gmail.com>
2022-11-07 19:06:25 +0530

    feat: search box in admin section [student,teacher]

--- src/pages/admin/ViewStudents.jsx
+++ src/pages/admin/ViewStudents.jsx
@@ -16,12 +16,46 @@ export default function ViewStudents(){
 
     if(!content) return null;
 
+    function Search() {
+        // Declare variables
+        var input, filter, tBody,tRow,tData, i, txtValue;
+        input = document.getElementById('search');
+        filter = input.value.toUpperCase();
+        // ul = document.getElementById("myUL");
+        tBody = document.getElementById('table-body');
+        tRow = tBody.getElementsByTagName('tr');
+      
+        // Loop through all list items, and hide those who don't match the search query
+        for (i = 0; i < tRow.length; i++) {
+          tData = tRow[i].getElementsByTagName("td")[1];
+          txtValue = tData.textContent || tData.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tRow[i].style.display = "";
+          } else {
+            tRow[i].style.display = "none";
+          }
+        }
+      }
+
     return(
         <>
         <p className="text-3xl p-10 font-semibold text-center">
             VIEW STUDENTS
         </p>
         <div className="p-6 m-auto w-1/2">
+
+
+<form>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+    <div class="relative">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input type="search" onKeyUp={Search} id="search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search by Enrollemtent number..."/>
+    </div>
+</form>
+
+
             <table className="w-full border">
                 <thead>
                     <tr className="bg-gray-50 border-b">
@@ -47,12 +81,11 @@ export default function ViewStudents(){
                         </th>
                     </tr>
                 </thead>
-                <tbody>
-                    {/* <NavLink></NavLink> */}
+                <tbody id="table-body">
                     {
                         content.map((element)=>{
                             return (
-                    <tr className="text-center border-b text-sm ">
+                    <tr id={element._id} className="text-center border-b text-sm ">
                         <td className="p-2 border-r">{element.name}</td>
                         <td className="p-2 border-r">{element.enrollment_no}</td>
                         <td className="p-2 border-r">{element.mobile_no}</td>
--- src/pages/admin/ViewTeachers.jsx
+++ src/pages/admin/ViewTeachers.jsx
@@ -16,12 +16,39 @@ export default function ViewTeachers(){
 
     if(!content) return null;
 
+    function Search() {
+        var input, filter, tBody,tRow,tData, i, txtValue;
+        input = document.getElementById('search');
+        filter = input.value.toUpperCase();
+        tBody = document.getElementById('table-body');
+        tRow = tBody.getElementsByTagName('tr');
+      
+        for (i = 0; i < tRow.length; i++) {
+          tData = tRow[i].getElementsByTagName("td")[1];
+          txtValue = tData.textContent || tData.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tRow[i].style.display = "";
+          } else {
+            tRow[i].style.display = "none";
+          }
+        }
+      }
+
     return(
         <>
         <p className="text-3xl p-10 font-semibold text-center">
             VIEW TEACHERS
         </p>
         <div className="p-6 m-auto w-1/2">
+        <form>   
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+    <div class="relative">
+        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input type="search" onKeyUp={Search} id="search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search by Enrollemtent number..."/>
+    </div>
+</form>
             <table className="w-full border">
                 <thead>
                     <tr className="bg-gray-50 border-b">
@@ -47,8 +74,7 @@ export default function ViewTeachers(){
                         </th>
                     </tr>
                 </thead>
-                <tbody>
-                    {/* <NavLink></NavLink> */}
+                <tbody id="table-body">
                     {
                         content.map((element)=>{
                             return (
