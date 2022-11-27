d491f5b530108c313e9fc9aa50aaf112c80ab337
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-11-27 10:05:50 +0530

    feat: button to assess marks for teacher

--- src/pages/teacher/TeacherAllocatedStudents.jsx
+++ src/pages/teacher/TeacherAllocatedStudents.jsx
@@ -1,4 +1,5 @@
 import React, { useState,useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import axios from 'axios';
 
 export default function TeacherAllocatedStudents(){
@@ -16,7 +17,7 @@ export default function TeacherAllocatedStudents(){
     },[])
     if(!content) return null;
 
-    const handleSubmit = (e) => {
+    const handleIndustrySubmit = (e) => {
         axios
         .post("http://localhost:9000/teacher/send-email", inputs)
         .then((res) => {
@@ -36,6 +37,9 @@ export default function TeacherAllocatedStudents(){
             ALLOCATED STUDENTS
         </p>
         <div className="p-6 m-auto w-1/2">
+            <div className="items-center py-2 pl-[768px]">
+                <button className="bg-gray-900 p-1 text-gray-50 rounded">ASK ASSESSMENT</button>
+            </div>
             <table className="w-full border">
                 <thead>
                     <tr className="bg-gray-50 border-b">
@@ -75,10 +79,12 @@ export default function TeacherAllocatedStudents(){
                             <td className="p-2 border-r">{element.enrollment_no}</td>
                             <td className="p-2 border-r">{element.mobile_no}</td>
                             <td className="p-2 border-r">{element.organization_mentor_email}</td>
-                            <td className="p-2 border-r" onClick={() => {
+                            <td className="p-2 border-r">
+                                <NavLink target={"__blank"} to={`/give-assessment/?name=${element.name}`}><button className="bg-gray-900 text-gray-50 p-1 mr-2 rounded">GIVE</button></NavLink>
+                            <button className="bg-gray-900 text-gray-50 p-1 ml-2 rounded" onClick={() => {
                                 setInputs(element); 
-                                handleSubmit()
-                            }}><button>ASK ASSESSMENT</button></td>
+                                handleIndustrySubmit()
+                            }}>ASK</button></td>
                         </tr>
                         )
                     })}
