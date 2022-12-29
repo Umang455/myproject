438e07b35bcf3ee4672febeadf03b7f336666232
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-29 18:50:20 +0530

    feat: new landing page

--- src/pages/HeroSection.jsx
+++ src/pages/HeroSection.jsx
@@ -4,57 +4,19 @@ import { NavLink } from "react-router-dom";
 export default function HeroSection() {
     return (
         <>
-        <div className="mt-32 flex flex-row mx-32">
-            <form className="basis-1/3">
-                <div className="w-1/2 p-6 m-auto bg-gray-300 rounded-md">
-                    <p className="text-3xl font-semibold text-center text-gray-900">
-                        Admin Login
-                    </p>
-                    <form>
-                        <div className="mt-6">
-                        <NavLink to="/admin/login">
-                            <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-slate-700">
-                                LOGIN
-                                </button>
-                        </NavLink>
-                        </div>
-                    </form>
-                </div>
-            </form>
-
-            <form className="basis-1/3">
-                <div className="w-1/2 p-6 m-auto bg-gray-300 rounded-md">
-                    <p className="text-3xl font-semibold text-center text-gray-900">
-                        Teacher Login
-                    </p>
-                    <form>
-                        <div className="mt-6">
-                            <NavLink to="/teacher/login">
-                                <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-slate-700">
-                                LOGIN
-                                </button>
-                            </NavLink>
-                        </div>
-                    </form>
-                </div>
-            </form>
-
-            <form className="basis-1/3">
-                <div className="w-1/2 p-6 m-auto bg-gray-300 rounded-md ">
-                    <p className="text-3xl font-semibold text-center text-gray-900">
-                        Student Login
-                    </p>
-                    <form>
-                        <div className="mt-6">
-                            <NavLink to="/student/login">
-                                <button className="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-900 rounded-md hover:bg-slate-700">
-                                LOGIN
-                                </button>
-                            </NavLink>
-                        </div>
-                    </form>
-                </div>
-            </form>
+        <div className="bg-gradient-to-r from-indigo-300 min-h-[94%] sticky top-0 overflow-hidden to-red-200 flex flex-col justify-center text-center text-black items-center h-3/4">
+          <h1 className="md:text-5xl text-7xl font-semibold py-2">
+          Government Polytechnic, Mumbai
+          </h1>
+          <h3 className="text-5xl font-medium py-4">Inplant Training Portal</h3>
+          <h2 className="text-2xl font-medium py-4">An online communication tool for inplant training portal</h2>
+          <div className="text-xl">
+          <NavLink to="/login">
+            <button className="px-6 py-2 bg-purple-800 border-2 border-purple-800 hover:text-purple-800 text-white rounded-lg hover:bg-transparent border-2 shadow-lg">
+            Get Started
+            </button>
+            </NavLink>
+          </div>
         </div>
         </>
     );
