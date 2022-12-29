bc0022efcf5ee81e7f47afd37a9bc29876a9783f
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-12-29 18:44:24 +0530

    feat: create and route all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,78 @@
-import React from "react"
+import React, { useState } from "react"
 import { Routes, Route } from "react-router-dom"
-import Head from "./components/Head"
-import HeroNavBar from "./components/HeroNavBar"
+
+import { Token } from "./context"
+
+// hero components
+import HeroNavBar from "./components/Navbar/HeroNavBar/HeroNavbar"
 import HeroSection from "./pages/HeroSection"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
-import AdminLogin from "./pages/AdminLogin"
-import TeacherLogin from "./pages/TeacherLogin"
-import StudentLogin from "./pages/StudentLogin"
-import AdminNavBar from "./components/AdminNavBar"
+import Login from "./components/Login"
+
+// admin components
+import AdminNavbar from "./components/Navbar/AdminNavBar/AdminNavbar"
 import AdminDashboard from "./pages/admin/AdminDashboard"
-import AdminStudent from "./pages/admin/AdminStudent"
-import AddStudent from "./pages/admin/AddStudent"
-import DeleteStudent from "./pages/admin/DeleteStudent"
-import ViewStudents from "./pages/admin/ViewStudents"
-import AdminTeacher from "./pages/admin/AdminTeacher"
 import AddTeacher from "./pages/admin/AddTeacher"
 import DeleteTeacher from "./pages/admin/DeleteTeacher"
 import ViewTeachers from "./pages/admin/ViewTeachers"
-import AdminMentor from "./pages/admin/AdminMentor"
+import AddStudent from "./pages/admin/AddStudent"
+import DeleteStudent from "./pages/admin/DeleteStudent"
+import ViewStudents from "./pages/admin/ViewStudents"
 import AllocateMentor from "./pages/admin/AllocateMentor"
 import ViewMentors from "./pages/admin/ViewMentors"
-import TeacherNavBar from "./components/TeacherNavBar"
+
+// teacher components
+import TeacherNavbar from "./components/Navbar/TeacherNavBar/TeacherNavbar"
 import TeacherDashboard from "./pages/teacher/TeacherDashboard"
 import TeacherStudent from "./pages/teacher/TeacherStudent"
 import TeacherAllocatedStudents from "./pages/teacher/TeacherAllocatedStudents"
-import StudentNavBar from "./components/StudentNavBar"
+
+// student components
+import StudentNavbar from "./components/Navbar/StudentNavBar/StudentNavBar"
 import StudentDashboard from "./pages/student/StudentDashboard"
-import StudentDocuments from "./pages/student/StudentDocuments"
 import StudentUploadDocuments from "./pages/student/StudentUploadDocuments"
-import AdminChangeInfo from "./pages/admin/AdminChangeInfo"
-import AdminChangePassword from "./pages/admin/AdminChangePassword"
-import TeacherChangeInfo from "./pages/teacher/TeacherChangeInfo"
-import TeacherChangePassword from "./pages/teacher/TeacherChangePassword"
-import StudentChangeInfo from "./pages/student/StudentChangeInfo"
-import StudentChangePassword from "./pages/student/StudentChangePassword"
-import StudentProfile from "./components/StudentProfile"
-import IndustryForm from "./components/IndustryForm"
-import FacultyForm from "./components/FacultyForm"
+import StudentDocuments from "./pages/student/StudentDocuments"
 
 export default function App() {
+
+  const [token, setToken] = useState('')
+
   return (
     <>
+    <Token.Provider value={[token, setToken]}>
+    <section className="h-screen font-[Popins] md:bg-top bg-center">
     <Routes>
-    <Route exact path="/admin/student-profile" element={[<Head/>,<AdminNavBar/>,<StudentProfile />]} />
-    <Route exact path="/teacher/student-profile" element={[<Head/>,<TeacherNavBar/>,<StudentProfile />]} />
-      <Route exact path="/" element={[<Head />, <HeroNavBar />, <HeroSection />]} />
-      <Route exact path="/about" element={[<Head />, <HeroNavBar />, <About />]} />
-      <Route exact path="/contact" element={[<Head />, <HeroNavBar />, <Contact />]} />
-      <Route exact path="/admin/login" element={[<Head />, <HeroNavBar />, <AdminLogin />]} />
-      <Route exact path="/teacher/login" element={[<Head />, <HeroNavBar />, <TeacherLogin />]} />
-      <Route exact path="/student/login" element={[<Head />, <HeroNavBar />, <StudentLogin />]} />
-      <Route exact path="/admin/dashboard" element={[<Head />, <AdminNavBar />, <AdminDashboard />]} />
-      <Route exact path="/admin/student" element={[<Head />, <AdminNavBar />, <AdminStudent />]} />
-      <Route exact path="/admin/student/add-student" element={[<Head />, <AdminNavBar />, <AddStudent />]} />
-      <Route exact path="/admin/student/delete-student" element={[<Head />, <AdminNavBar />, <DeleteStudent />]} />
-      <Route exact path="/admin/student/student-list" element={[<Head />, <AdminNavBar />, <ViewStudents />]} />
-      <Route exact path="/admin/teacher" element={[<Head />, <AdminNavBar />, <AdminTeacher />]} />
-      <Route exact path="/admin/teacher/add-teacher" element={[<Head />, <AdminNavBar />, <AddTeacher />]} />
-      <Route exact path="/admin/teacher/delete-teacher" element={[<Head />, <AdminNavBar />, <DeleteTeacher />]} />
-      <Route exact path="/admin/teacher/teacher-list" element={[<Head />, <AdminNavBar />, <ViewTeachers />]} />
-      <Route exact path="/admin/change-info" element={[<Head />, <AdminNavBar />, <AdminChangeInfo />]} />
-      <Route exact path="/admin/change-password" element={[<Head />, <AdminNavBar />, <AdminChangePassword />]} />
-      <Route exact path="/admin/mentor" element={[<Head />, <AdminNavBar />, <AdminMentor />]} />
-      <Route exact path="/admin/mentor/allocate-mentor" element={[<Head />, <AdminNavBar />, <AllocateMentor />]} />
-      <Route exact path="/admin/mentor/mentor-list" element={[<Head />, <AdminNavBar />, <ViewMentors />]} />
-      <Route exact path="/teacher/dashboard" element={[<Head />, <TeacherNavBar />, <TeacherDashboard />]} />
-      <Route exact path="/teacher/student" element={[<Head />, <TeacherNavBar />, <TeacherStudent />]} />
-      <Route exact path="/teacher/allocated-students" element={[<Head />, <TeacherNavBar />, <TeacherAllocatedStudents />]} />
-      <Route exact path="/teacher/change-info" element={[<Head />, <TeacherNavBar />, <TeacherChangeInfo />]} />
-      <Route exact path="/teacher/change-password" element={[<Head />, <TeacherNavBar />, <TeacherChangePassword />]} />
-      <Route exact path="/student/dashboard" element={[<Head />, <StudentNavBar />, <StudentDashboard />]} />
-      <Route exact path="/student/documents" element={[<Head />, <StudentNavBar />, <StudentDocuments />]} />
-      <Route exact path="/student/upload-documents" element={[<Head />, <StudentNavBar />, <StudentUploadDocuments />]} />
-      <Route exact path="/student/change-info" element={[<Head />, <StudentNavBar />, <StudentChangeInfo />]} />
-      <Route exact path="/student/change-password" element={[<Head />, <StudentNavBar />, <StudentChangePassword />]} />
-      <Route exact path="/ask-assessment" element={[<Head />, <IndustryForm />]} />
-      <Route exact path="/give-assessment" element={[<Head />, <FacultyForm />]} />
+      {/* Hero Section */}
+      <Route exact path="/" element={[<HeroNavBar />, <HeroSection />]} />
+      <Route exact path="/about" element={[<HeroNavBar />, <About />]} />
+      <Route exact path="/contact" element={[<HeroNavBar />, <Contact />]} />
+      <Route exact path="/login" element={[<HeroNavBar />, <Login />]} />
+
+      {/* Admin Section */}
+      <Route exact path="/admin/dashboard" element={[<AdminNavbar />, <AdminDashboard />]} />
+      <Route exact path="/admin/add-teacher" element={[<AdminNavbar />, <AddTeacher />]} />
+      <Route exact path="/admin/delete-teacher" element={[<AdminNavbar />, <DeleteTeacher />]} />
+      <Route exact path="/admin/view-teachers" element={[<AdminNavbar />, <ViewTeachers />]} />
+      <Route exact path="/admin/add-student" element={[<AdminNavbar />, <AddStudent />]} />
+      <Route exact path="/admin/delete-student" element={[<AdminNavbar />, <DeleteStudent />]} />
+      <Route exact path="/admin/view-students" element={[<AdminNavbar />, <ViewStudents />]} />
+      <Route exact path="/admin/allocate-mentors" element={[<AdminNavbar />, <AllocateMentor />]} />
+      <Route exact path="/admin/view-mentors" element={[<AdminNavbar />, <ViewMentors />]} />
+
+      {/* Teacher Section */}
+      <Route exact path="/teacher/dashboard" element={[<TeacherNavbar />, <TeacherDashboard />]} />
+      <Route exact path="/teacher/students" element={[<TeacherNavbar />, <TeacherStudent />]} />
+      <Route exact path="/teacher/allocated-students" element={[<TeacherNavbar />, <TeacherAllocatedStudents />]} />
+
+      {/* Student Section */}
+      <Route exact path="/student/dashboard" element={[<StudentNavbar />, <StudentDashboard />]} />
+      <Route exact path="/student/upload-documents" element={[<StudentNavbar />, <StudentUploadDocuments />]} />
+      <Route exact path="/student/documents" element={[<StudentNavbar />, <StudentDocuments />]} />
+
     </Routes>
+    </section>
+    </Token.Provider>
     </>
   )
 }
