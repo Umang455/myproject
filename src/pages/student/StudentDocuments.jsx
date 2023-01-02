dd3087f42e9b02576f2d08ae6b5388253b9b2ade
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-01-02 07:27:06 +0530

    chore: new styling for documents

--- src/pages/student/StudentDocuments.jsx
+++ src/pages/student/StudentDocuments.jsx
@@ -1,61 +1,95 @@
-import axios from "axios";
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 
 export default function StudentDocuments() {
     const [files, setFiles] = useState({})
     const token = JSON.parse(sessionStorage.getItem("token"))
-
+    const navigate = useNavigate()
+    
     useEffect(() => {
         axios
         .get("http://localhost:9000/student/file/map", {
             headers: {
-                Authorization: token
+                Authorization: "Bearer " + token
             }
         })
         .then((res) => {
+          if(res.data.documents) {
             setFiles(res.data.documents)
+          }
+        })
+        .catch((err) => {
+          navigate("/login");
         })
     }, [])
     if(!files) return null
 
-    console.log(files);
+    const handleDelete = (fileId) => {
+      const input = {
+        fileId: fileId
+      }
+      axios.post("http://localhost:9000/student/file/delete", input, {
+        headers: {
+          Authorization: token
+        }
+      })
+      .then((result)=>{
+          alert(result.data.message)
+          console.log(result)
+      })
+      .catch((err)=>{
+          alert(err.data.message||"Something went wrong")
+      })
+    }
     return (
-      <section className="section flex justify-center md:my-[10rem]" >
+      <section className="bg-gradient-to-r from-indigo-300 to-red-200 md:text-gray-300 h-[77.4rem] md:h-[55rem] md:pt-10 md:pb-10">
+        <div className="section flex justify-center md:my-[10rem]" >
           <div className="container ">
-  
-      <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-        <div className="px-4 py-5 sm:px-6">
-          <h3 className="text-lg font-medium leading-6 text-gray-900">Documents</h3>
-          <p className="mt-1 max-w-2xl text-sm text-gray-500">Download this documents</p>
-        </div>
-      
-            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm font-medium text-gray-500">Attachments</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
-                <ul role="list" className="divide-y divide-gray-200 rounded-md border border-gray-200">
-                {
-                    files.files?.map((file) => (
-                        <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
-                    <div className="flex w-0 flex-1 items-center">
-                      <div className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                      <span className="ml-2 w-0 flex-1 truncate">{file.name}</span>
-                    </div>
-                    <div className="ml-4 flex-shrink-0">
-                      <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                        Download
-                      </a>
-                    </div>
-                  </li>
-
-                    ))
-                }
-
-                </ul>
-              </dd>
+            <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+              <div className="px-4 py-5 sm:px-6">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">Documents</h3>
+                <p className="mt-1 max-w-2xl text-sm text-gray-500">Download this documents</p>
+              </div>
+              
+              <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">Attachments</dt>
+                  <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
+                    <ul role="list" className="divide-y divide-gray-200 rounded-md border border-gray-200">
+                      {
+                        files.files?.map((file) => (
+                          <li className="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                            <div className="flex w-0 flex-1 items-center">
+                              <div className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+                                <span className="ml-2 w-0 flex-1 truncate">{file.name}</span>
+                            </div>
+                            <div className="ml-4 flex-shrink-0">
+                              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                Download
+                              </a>
+                            </div>
+                            <div className="ml-4 flex-shrink-0">
+                              <a href={file.url} target="_blank" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                View
+                              </a>
+                            </div>
+                            <div className="ml-4 flex-shrink-0">
+                              <button onClick={() => {
+                                handleDelete(file.fileId)
+                              }} className="font-medium text-indigo-600 hover:text-indigo-500">
+                                Delete
+                              </button>
+                            </div>
+                          </li>
+                        ))
+                      }
+                    </ul>
+                  </dd>
+              </div>
             </div>
-
+          </div>
         </div>
-      </div>
       </section>
     )
   }
--- src/pages/student/StudentUploadDocuments.jsx
+++ src/pages/student/StudentUploadDocuments.jsx
@@ -1,21 +1,37 @@
-import React from "react";
-import axios from 'axios';
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function StudentUploadDocuments() {
-
     const [data,setData] = useState()
     const token = JSON.parse(sessionStorage.getItem("token"))
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        return () => {
+          axios
+          .get("http://localhost:9000/student", {
+            headers: {
+              Authorization: "Bearer " + token
+            }
+          })
+          .then((res) => {
+            console.log(res.data.message);
+          })
+          .catch((err) => {
+            navigate("/login");
+          })
+        }
+    }, [])
 
     const handleSubmit = (e) => {
         e.preventDefault()
         let formData = new FormData()
         formData.append('file', data)
       const url = `http://localhost:9000/student/file/upload`
-      console.log(localStorage.getItem('userId'));
         axios.post(url,formData, {
             headers: {
-                Authorization: token
+                Authorization: "Bearer " + token
             }
         })
         .then((result)=>{
@@ -29,22 +45,24 @@ export default function StudentUploadDocuments() {
 
     return (
         <>
-        <p className="text-3xl p-10 font-semibold text-center">
+        <section className="bg-gradient-to-r from-indigo-300 to-red-200 md:text-gray-300 h-[77.4rem] md:h-[55rem] md:pt-10 md:pb-10">
+        <p className="text-3xl p-10 text-gray-900 font-semibold text-center">
              UPLOAD DOCUMENT
         </p>
-            <form onSubmit={handleSubmit}>
-                <div className="flex justify-center items-center w-full">
-                    <label for="dropzone-file" className="flex flex-col justify-center items-center w-3/5 h-64 bg-gray-50 rounded-lg border-2 border-gray-300 border-dashed cursor-pointer">
-                        <div className="flex flex-col justify-center items-center pt-5 pb-6">
-                            <svg aria-hidden="true" className="mb-3 w-10 h-10 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
-                            <p className="mb-2 text-sm text-gray-500 "><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                            <p className="text-xs text-gray-500">(ENROLLMENT_NO-<i>filename</i>).pdf</p>
-                        </div>
-                        <input required id="dropzone-file" onChange={(e)=>{setData(e.target.files[0])}} name="file" type="file" accept="application/pdf"/>
-                        <button type="submit" className="bg-green-200 px-3 py-1 mt-4 rounded-lg border-2 border-gray-200">Upload</button>
-                    </label>
-                </div>
-            </form>
+        <form onSubmit={handleSubmit}>
+            <div className="flex justify-center items-center w-full">
+                <label for="dropzone-file" className="flex flex-col justify-center items-center w-3/5 h-64 bg-gray-900 rounded-lg border-2 border-gray-900 border-dashed cursor-pointer">
+                    <div className="flex flex-col justify-center items-center pt-5 pb-6">
+                        <svg aria-hidden="true" className="mb-3 w-10 h-10 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path></svg>
+                        <p className="mb-2 text-sm text-white "><span className="font-semibold">Click to upload</span></p>
+                        <p className="text-xs text-white">(ENROLLMENT_NO-<i>filename</i>).pdf</p>
+                    </div>
+                    <input required id="dropzone-file" onChange={(e)=>{setData(e.target.files[0])}} name="file" type="file" accept="application/pdf"/>
+                    <button type="submit" className="bg-purple-800 px-3 py-1 mt-4 rounded-lg border-2 border-purple-800 hover:bg-white hover:text-gray-900">Upload</button>
+                </label>
+            </div>
+        </form>
+        </section>
         </>
     );
 }
