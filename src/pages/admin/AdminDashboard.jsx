d3bd379bf15bd8b3b52ef74e25539d38aaae2399
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-01-02 11:08:43 +0530

    prev dashbiard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,111 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom"; 
 
 export default function AdminDashboard() {
-  const [content, setContent] = useState({})
-  const token = JSON.parse(sessionStorage.getItem("token"))
-  const navigate = useNavigate()
+    const [content, setContent] = useState({});
+    const [authenticated, setauthenticated] = useState(null);
 
-  useEffect(() => {
-    return () => {
-      axios
-      .get("https://inplantportal.onrender.com/admin", {
-        headers: {
-          Authorization: "Bearer " + token
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("authenticated");
+        if (loggedInUser) {
+            setauthenticated(loggedInUser);
         }
-      })
-      .then((res) => {
-        if(sessionStorage.getItem("info") != "") {
-          setContent(JSON.parse(sessionStorage.getItem("info")))
+        if(localStorage.getItem("info") != ""){
+            localStorage.getItem('info')
+            setContent(JSON.parse(localStorage.getItem('info')));
+        }
+    }, []);
 
-
-         }
-
-      })
-      .catch((err) => {
-        navigate("/login");
-      })
+    if (authenticated == "false") {
+        return <Navigate replace to="/admin/login" />;
+    } else {
+        return (
+            <>
+            <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+                    <div className="px-4 py-6 sm:px-0">
+                        <div className="h-auto rounded-lg border-4 border-dashed border-gray-200">
+                            <div className="overflow-hidden bg-white shadow sm:rounded-lg">
+                                <div className="px-4 py-5 sm:px-6">
+                                    <p className="text-lg font-medium leading-6 text-gray-900">Information</p>
+                                    <p className="mt-1 max-w-2xl text-sm text-gray-500">Personal details</p>
+                                </div>
+                                <div className="border-t border-gray-200">
+                                    <dl>
+                                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                            <dt className="text-sm font-medium text-gray-500">Full name</dt>
+                                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{content.name || ""}</dd>
+                                        </div>
+                                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                            <dt className="text-sm font-medium text-gray-500">Designation</dt>
+                                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">Root</dd>
+                                        </div>
+                                        <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                            <dt className="text-sm font-medium text-gray-500">Email address</dt>
+                                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">{content.email || ""}</dd>
+                                        </div>
+                                        <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                            <dt className="text-sm font-medium text-gray-500">Contact No.</dt>
+                                            <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">+91 {content.mobile_no || ""}</dd>
+                                        </div>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
     }
-  }, [])
-
-  return( content && (
-  <>
-  <section className="bg-gradient-to-r from-indigo-300 to-red-200 md:text-gray-300 h-[77.4rem] md:h-[55.5rem] md:pt-10 md:pb-10">
-    <div className=" md:mx-[8rem] md:bg-gray-900">
-      <div className="flex flex-col min-w-0 break-words w-full mb-6  -lg rounded-lg bg-blueGray-100 border-0">
-        <div className="rounded-t bg-transparent md:text-white md:bg-gray-900 mb-0 px-6 py-6">
-          <div className="text-center  flex justify-between">
-            <h6 className="text-xl md:text-purple-500 font-bold">Dashboard</h6>
-          </div>
-            <hr className="mt-6 border-b-1 border-black md:border-gray-300" />
-        </div>
-        
-        <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <div>
-            <h6 className="text-blueGray-400 md:text-white text-sm mt-3 mb-6 font-bold uppercase">
-              User Information
-            </h6>
-
-            <div className="flex flex-wrap">
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="w-full mb-3">
-                  <label
-                  className="block uppercase text-blueGray-600  text-xs font-bold mb-2">
-                    Username
-                  </label>
-                  <p
-                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-red-200 rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
-                    {content.username || ""}
-                  </p>
-                </div>
-              </div>
-              
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="  w-full mb-3">
-                  <label
-                  className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
-                    Full Name
-                  </label>
-                  <p
-                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-red-200 rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
-                    {content.name || ""}
-                  </p>
-                </div>
-              </div>
-              
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="  w-full mb-3">
-                  <label
-                  className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
-                    Email Address
-                  </label>
-                  <p
-                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-red-200 rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
-                    {content.email || ""}
-                  </p>
-                </div>
-              </div>
-              
-              <div className="w-full lg:w-6/12 px-4">
-                <div className="  w-full mb-3">
-                  <label
-                  className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
-                    Mobile No
-                  </label>
-                  <p
-                  className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 rounded text-sm md:text-red-200  focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
-                    {content.mobile_no || ""}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  </>
-  )
-  );
-}
+};
