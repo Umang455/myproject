cdafed4ac3bed643f6b8bbe23673228d9ece1454
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-01-02 07:27:50 +0530

    feat: button for faculty mentor to give marks

--- src/pages/teacher/TeacherAllocatedStudents.jsx
+++ src/pages/teacher/TeacherAllocatedStudents.jsx
@@ -1,22 +1,24 @@
-import React, { useState,useEffect, useContext } from "react";
+import React, {useState,useEffect} from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import axios from 'axios';
-// import { Token } from "../../context";
 
-export default function TeacherAllocatedStudents(){
-  const [content,setContent] = useState([]);
-
+export default function AllocateMentor(){
+  const [content,setContent] = useState(null);
+  const bulkInputs = [];
   const token = JSON.parse(sessionStorage.getItem("token"))
-//   const [token, setToken] = useContext(Token)
-  console.log(token);
+  const navigate = useNavigate()
   
   useEffect(()=>{
     axios.get('http://localhost:9000/teacher/allocated-students', {
-        headers: {
-            Authorization: token
-        }
+      headers: {
+        Authorization: "Bearer " + token
+      }
     })
     .then((res)=>{
       setContent(res.data.students)
+    })
+    .catch((err) => {
+      navigate("/login");
     })
   },[])
   
@@ -39,95 +41,138 @@ export default function TeacherAllocatedStudents(){
       }
     }
   }
-  
-  const handleChanges = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setInputs(values => ({ ...values, [name]: value }));
-    console.log(inputs);
-  };
-  
-  console.log(content);
+
+  const handleIndustrySubmit = (studentName) => {
+    const input = {
+      studentName: studentName
+    }
+    axios
+    .post("http://localhost:9000/teacher/send-email", input, {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    })
+    .then((res) => {
+      alert(res.data.message);
+    })
+    .catch((err) => {
+      console.log('error : ',err);
+      alert(err.response.data.message);
+    });
+  }
+
+  const handleBulkMail = (e) => {
+    axios
+    .post("http://localhost:9000/teacher/send-bulk-email", bulkInputs, {
+      headers: {
+        Authorization: "Bearer " + token
+      }
+    })
+    .then((res) => {
+      alert(res.data.message);
+    })
+    .catch((err) => {
+      console.log('error : ',err);
+      alert(err.response.data.message);
+    });
+  }
   
   return(
-    <>
-    <div className="p-6 m-auto w-full">
-      <form>
-        <label for="default-search" class="text-sm font-medium text-gray-900 sr-only ">
-          Search
-        </label>
-        <input type="search" onKeyUp={Search} id="search" class="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search by Enrollemtent number..."/>
-      </form>
-    </div>
+  <>
+  <section className="bg-gradient-to-r from-indigo-300 to-red-200 md:h-[55.5rem] md:px-[10rem] md:py-[4rem] md:w-auto">
+    <div className="p-2 md:rounded">
+      <div className="p-6 m-auto w-full">
+        <form>
+          <label htmlFor="default-search" className="text-sm font-medium text-gray-100 bg-gray-900 sr-only ">
+            Search
+          </label>
+          <input type="search" onKeyUp={Search} id="search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg focus:ring-blue-500 focus:border-blue-500 " placeholder="Search by Enrollemtent number..."/>
+        </form>
+      </div>
 
-    <div className="px-6 py-2">
-    <table className="min-w-full border-collapse block md:table">
+      <div className="py-2" style={{
+          display: "flex",
+          justifyContent: "end",
+          alignContent: "end"
+        }} >
+        <button onClick={handleBulkMail} className="bg-gray-900 border border-gray-900 text-white hover:bg-white hover:text-black  p-1 rounded">
+          ASK ASSESSMENT
+        </button>
+      </div>
+      
+      <table className="md:table w-full" >
         <thead className="md:block md:table-header-group hidden">
-        <tr className="border border-grey-500 md:border-none block md:table-row -top-full md:top-auto -left-full md:left-auto">
-            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
-            Enrollment No
+          <tr className="border md:border-none block md:table-row -top-full md:top-auto -left-full md:left-auto">
+            <th className="bg-gray-900 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
+              Enrollment No.
             </th>
-            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
-            Name
+            <th className="bg-gray-900 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
+              Full Name
             </th>
-            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
-            Email Id
+            <th className="bg-gray-900 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
+              Email Id
             </th>
-            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
-            Mobile Number
+            <th className="bg-gray-900 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
+              Mobile No.
             </th>
-            <th className="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
-            Actions
-          </th>
-        </tr>
+            <th className="bg-gray-900 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell sm:hidden">
+              Action
+            </th>
+          </tr>
         </thead>
         <tbody id="table-body" className="block md:table-row-group">
         {
-        content.map((element) => {
+          content.map((element) => {
+            bulkInputs.push(element.name)
             return(
-            <tr key={element._id} id="tr" className="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+              <tr id="tr" className="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+                  <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                     <span className="inline-block w-1/3 md:hidden font-bold">
-                    Enrollment No
+                      Enrollment No
                     </span>
                     {element.enrollment_no}
-                </td>
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                  </td>
+                  <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                     <span className="inline-block w-1/3 md:hidden font-bold">
-                    Name
+                      Name
                     </span>
                     {element.name}
-                </td>
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                  </td>
+                  <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                     <span className="inline-block w-1/3 md:hidden font-bold">
-                    Email Id
+                      Email Id
                     </span>
                     {element.email}
-                </td>
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                  </td>
+                  <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
                     <span className="inline-block w-1/3 md:hidden font-bold">
-                    Mobile Number
+                      Mobile Number
                     </span>
                     {element.mobile_no}
-                </td>
-                <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
-                <span className="inline-block w-1/3 md:hidden font-bold">
-                  Actions
-                </span>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 mr-4 border border-blue-500 rounded">
-                  Edit
-                </button>
-                <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">
-                  Delete
-                </button>
-              </td>
-            </tr>
+                  </td>
+                  <td className="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                    <span className="inline-block w-1/3 md:hidden font-bold">
+                      Allocate
+                    </span>
+                    <NavLink to={`/give-assessment/?name=${element.name}`} target="__blank">
+                      <button className="bg-purple-800 border-purple-800 text-white hover:bg-white hover:text-purple-800 font-bold py-1 px-2 border rounded mr-2">
+                        GIVE
+                      </button>
+                    </NavLink>
+                    <button onClick={() => {
+                      handleIndustrySubmit(element.name)
+                    }} className="bg-gray-900 border-gray-900 text-white hover:bg-white hover:text-gray-900 font-bold py-1 px-2 border rounded ml-2">
+                      ASK
+                    </button>
+                  </td>
+              </tr>
             )
-        })
+          })
         }
-    </tbody>
-    </table>
+        </tbody>
+      </table>
     </div>
+  </section>
   </>
   )
 }
