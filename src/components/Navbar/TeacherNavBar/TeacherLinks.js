b96e65ae09629636006d12c8e8e0213d6055bdc9
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-01-02 07:19:36 +0530

    feat:  new navbar for teacher

---
+++ src/components/Navbar/TeacherNavBar/TeacherLinks.js
@@ -0,0 +1,14 @@
+export const links = [
+  {
+    name: "Settings",
+    submenu: true,
+    sublinks: [
+      {
+        sublink: [
+          { name: "Update Information", link: "/teacher/update-info" },
+          { name: "Change Passowrd", link: "/teacher/change-password" },
+        ]
+      }
+    ]
+  }
+];
---
+++ src/components/Navbar/TeacherNavBar/TeacherNavLinks.jsx
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { links } from "./TeacherLinks";
+
+const NavLinks = () => {
+  const [heading, setHeading] = useState("");
+  const [subHeading, setSubHeading] = useState("");
+  return (
+    <>
+      {links.map((link) => (
+        <div>
+          <div className="px-3 text-left md:cursor-pointer group  text-black md:text-gray-300 hover:md:text-purple-500">
+            <h1
+              className="py-7 flex justify-between items-center md:pr-0 pr-5 group "
+              onClick={() => {
+                heading !== link.name ? setHeading(link.name) : setHeading("");
+                setSubHeading("");
+              }}
+            >
+              {link.name}
+              <span className="text-xl md:hidden inline ">
+                <ion-icon
+                  name={`${
+                    heading === link.name ? "chevron-up" : "chevron-down"
+                  }`}
+                ></ion-icon>
+              </span>
+              <span className="text-xl md:mt-1 md:ml-2  md:block hidden group-hover:rotate-180 group-hover:-mt-2">
+                <ion-icon name="chevron-down"></ion-icon>
+              </span>
+            </h1>
+            {link.submenu && (
+              <div className="">
+                <div className="absolute top-20 hidden group-hover:md:block hover:md:block">
+                  <div className="py-3">
+                    <div
+                      className="w-4 h-4 left-3 absolute bg-purple-600
+                    mt-1 rotate-45"
+                    ></div>
+                  </div>
+                  <div className="p-5 grid grid-cols-1 bg-gray-800 gap-10">
+                    {link.sublinks.map((mysublinks) => (
+                      <div>
+                        {mysublinks.sublink.map((slink) => (
+                          <li className="text-sm hover:text-purple-600 text-white my-2.5">
+                            <NavLink
+                              to={slink.link}
+                              className="hover:text-primary"
+                            >
+                              {slink.name}
+                            </NavLink>
+                          </li>
+                        ))}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+          {/* Mobile menus */}
+          <div
+            className={`
+            ${heading === link.name ? "md:hidden" : "hidden"}
+          `}
+          >
+            {/* sublinks */}
+            {link.sublinks.map((slinks) => (
+              <div>
+                <div>
+                  {/* <h1
+                    onClick={() =>
+                      subHeading !== slinks.Head
+                        ? setSubHeading(slinks.Head)
+                        : setSubHeading("")
+                    }
+                    className="py-4 pl-7 font-semibold md:pr-0 pr-5 flex justify-between items-center md:pr-0 pr-5"
+                  >
+
+                    <span className="text-xl md:mt-1 md:ml-2 inline">
+                      <ion-icon
+                        name={`${
+                          subHeading === slinks.Head
+                            ? "chevron-up"
+                            : "chevron-down"
+                        }`}
+                      ></ion-icon>
+                    </span>
+                  </h1> */}
+                  <div>
+                    {slinks.sublink.map((slink) => (
+                      <li className="py-3 pl-14">
+                        <NavLink to={slink.link}>{slink.name}</NavLink>
+                      </li>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default NavLinks;
