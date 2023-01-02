22e36eb0e581695bee18f18d9a822fcca54ffe92
Pranav Dhawale <pranavdhawale19@gmail.com>
2023-01-02 07:26:40 +0530

    feat: create new pages for changeInfo & changePassword

--- src/pages/student/StudentChangeInfo.jsx
+++ src/pages/student/StudentChangeInfo.jsx
@@ -1,29 +1,48 @@
-import React, { useState } from "react";
+import React, {useState,useEffect} from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from 'axios';
 
 export default function StudentChangeInfo(){
     const [inputs, setInputs] = React.useState("");
+    const token = JSON.parse(sessionStorage.getItem("token"))
+    const navigate = useNavigate()
 
-    const [tempEmail, setTempEmail] = React.useState(JSON.parse(localStorage.getItem('info')).email);
-    const [tempMobileNo, setTempMobile] = React.useState(JSON.parse(localStorage.getItem('info')).mobile_no);
+    useEffect(() => {
+        return () => {
+          axios
+          .get("http://localhost:9000/student", {
+            headers: {
+              Authorization: "Bearer " + token
+            }
+          })
+          .then((res) => {
+            console.log(res.data.message);
+          })
+          .catch((err) => {
+            navigate("/login");
+          })
+        }
+      }, [])
 
     const handleChanges = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         setInputs(values => ({ ...values, [name]: value }));
+        console.log(inputs);
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
         axios
-            .post(`http://localhost:9000/student/update/info/${localStorage.getItem("userId")}`, inputs)
+            .post(`http://localhost:9000/student/update/info`, inputs, {
+                headers: {
+                    Authorization: "Bearer " + token
+                }
+            })
             .then((res) => {
                 alert(res.data.message);
-                localStorage.setItem("info", JSON.stringify(res.data.result));
-                console.log(res);
-
-                // navigate("/admin/dashboard");
+                sessionStorage.setItem("info", JSON.stringify(res.data.user));
+                navigate("/student/dashboard");
             })
             .catch((err) => {
                 console.log('error : ',err);
@@ -35,61 +54,69 @@ export default function StudentChangeInfo(){
 
     return(
         <>
-        <p className="text-3xl p-10 font-semibold text-center">
-             CHANGE INFO
-        </p>
-        <div className="p-6 m-auto bg-gray-100 rounded-md shadow-xl w-1/3">
-            <form onSubmit={handleSubmit}>
-                <div className="p-4">
-                    <div className="p-2">
-                        <label
-                        htmlFor="email"
-                        className="block text-xl">
-                            Email Id :
-                        </label>
-                        <div className="flex">
-                            <input
-                            type="email"
-                            name="email"
-                            value={tempEmail}
-                            autoComplete="on"
-                            onChange={(e) => {
-                                setTempEmail(e.target.value);
-                                handleChanges(e);
-                            }}
-                            className="block w-full px-4 py-2 mt-2 border"/>
+        <section className="bg-gradient-to-r from-indigo-300 to-red-200 md:text-gray-300 h-[77.4rem] md:h-[55.5rem] md:pt-10 md:pb-10">
+            <div className=" md:mx-[8rem] md:bg-gray-900">
+                <div className="flex flex-col min-w-0 break-words w-full mb-6  -lg rounded-lg bg-blueGray-100 border-0">
+                    <div className="rounded-t bg-transparent md:text-white md:bg-gray-900 mb-0 px-6 py-6">
+                        <div className="text-center  flex justify-between">
+                            <h6 className="text-xl md:text-purple-500 font-bold">Update Information</h6>
                         </div>
+                        <hr className="mt-6 border-b-1 border-black md:border-gray-300" />
                     </div>
-                    <div className="p-2">
-                        <label
-                        htmlFor="contact"
-                        className="block text-xl">
-                            Contact No :
-                        </label>
-                        <div className="flex">
-                            <input
-                            type="number"
-                            name="mobile_no"
-                            value={tempMobileNo}
-                            autoComplete="on"
-                            onChange={(e) => {
-                                setTempMobile(e.target.value);
-                                handleChanges(e);
-                            }}
-                            className="block w-full px-4 py-2 mt-2 border"/>
-                        </div>
+                    
+                    <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <h6 className="text-blueGray-400 md:text-white text-sm mt-3 mb-6 font-bold uppercase">
+                                    User Contact Information
+                                </h6>
+
+                                <div className="flex">
+                                    <div className="w-full lg:w-6/12 px-4">
+                                        <div className="w-full mb-3">
+                                            <label
+                                            className="block uppercase text-blueGray-600  text-xs font-bold mb-2">
+                                            Email Id
+                                            </label>
+                                            <input
+                                            name="email"
+                                            onChange={handleChanges}
+                                            type="email"
+                                            autoComplete="on"
+                                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-black rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="w-full lg:w-6/12 px-4">
+                                        <div className="  w-full mb-3">
+                                            <label
+                                            className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
+                                            Mobile Number
+                                            </label>
+                                            <input
+                                            name="mobile_no"
+                                            onChange={handleChanges}
+                                            type="number"
+                                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-black rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div style={{
+                                marginTop: 5,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignContent: "center"}}>
+                                    <button type="submit" className="text-lg font-semibold border-2 border-purple-800 bg-purple-700 text-white rounded-lg px-6 py-2 block shadow-xl hover:text-purple-700 hover:bg-white" >
+                                        Update
+                                    </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                
-                <div className="flex items-center justify-center">
-                    <button
-                    type="submit"
-                    className="items-center px-4 py-2 w-1/4 rounded-md text-white bg-gray-900">
-                        SAVE
-                    </button>
-                </div>
-            </form>
-        </div>
+            </div>
+        </section>
         </>
     )
 }
--- src/pages/student/StudentChangePassword.jsx
+++ src/pages/student/StudentChangePassword.jsx
@@ -1,9 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import axios from 'axios';
 
 export default function StudentChangePassword(){
-    const [inputs, setInputs] = React.useState("");
+    const [inputs, setInputs] = useState("");
+    const token = JSON.parse(sessionStorage.getItem("token"))
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        return () => {
+          axios
+          .get("http://localhost:9000/student", {
+            headers: {
+              Authorization: "Bearer " + token
+            }
+          })
+          .then((res) => {
+            console.log(res.data.message);
+          })
+          .catch((err) => {
+            navigate("/login");
+          })
+        }
+      }, [])
 
     const handleChanges = (e) => {
         const name = e.target.name;
@@ -14,13 +33,16 @@ export default function StudentChangePassword(){
     const handleSubmit = (e) => {
         e.preventDefault();
         axios
-            .post(`http://localhost:9000/student/update/password/${localStorage.getItem("userId")}`, inputs)
+            .post(`http://localhost:9000/student/update/password`, inputs, {
+                headers: {
+                    Authorization: "Bearer " + token
+                }
+            })
             .then((res) => {
                 alert(res.data.message);
-                localStorage.setItem("info", JSON.stringify(res.data.result));
-                console.log(res);
-
-                // navigate("/admin/dashboard");
+                sessionStorage.removeItem("token")
+                sessionStorage.removeItem("info")
+                navigate("/login");
             })
             .catch((err) => {
                 console.log('error : ',err);
@@ -28,69 +50,84 @@ export default function StudentChangePassword(){
             });
     };
 
-    
-
     return(
         <>
-        <p className="text-3xl p-10 font-semibold text-center">
-             CHANGE PASSWORD
-        </p>
-        <div className="p-6 m-auto bg-gray-100 rounded-md shadow-xl w-1/3">
-            <form onSubmit={handleSubmit}>
-                <div className="p-4">
-                    <div className="p-2">
-                        <label
-                        htmlFor="oldPassword"
-                        className="block text-xl">
-                            Old Password :
-                        </label>
-                        <div className="flex">
-                            <input
-                            type="password"
-                            name="oldPassword"
-                            onChange={handleChanges}
-                            className="block w-full px-4 py-2 mt-2 border"/>
+        <section className="bg-gradient-to-r from-indigo-300 to-red-200 md:text-gray-300 h-[77.4rem] md:h-[55.5rem] md:pt-10 md:pb-10">
+            <div className=" md:mx-[8rem] md:bg-gray-900">
+                <div className="flex flex-col min-w-0 break-words w-full mb-6  -lg rounded-lg bg-blueGray-100 border-0">
+                    <div className="rounded-t bg-transparent md:text-white md:bg-gray-900 mb-0 px-6 py-6">
+                        <div className="text-center  flex justify-between">
+                            <h6 className="text-xl md:text-purple-500 font-bold">Change Password</h6>
                         </div>
+                        <hr className="mt-6 border-b-1 border-black md:border-gray-300" />
                     </div>
-                    <div className="p-2">
-                        <label
-                        htmlFor="newPassword"
-                        className="block text-xl">
-                            New Password :
-                        </label>
-                        <div className="flex">
-                            <input
-                            type="password"
-                            name="newPassword"
-                            onChange={handleChanges}
-                            className="block w-full px-4 py-2 mt-2 border"/>
-                        </div>
-                    </div>
-                    <div className="p-2">
-                        <label
-                        htmlFor="confirmPassword"
-                        className="block text-xl">
-                            Confirm Password :
-                        </label>
-                        <div className="flex">
-                            <input
-                            type="password"
-                            name="confirmPassword"
-                            onChange={handleChanges}
-                            className="block w-full px-4 py-2 mt-2 border"/>
-                        </div>
+                    
+                    <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                        <form onSubmit={handleSubmit}>
+                            <div>
+                                <h6 className="text-blueGray-400 md:text-white text-sm mt-3 mb-6 font-bold uppercase">
+                                    Password Information
+                                </h6>
+
+                                <div className="flex">
+                                    <div className="w-full lg:w-6/12 px-4">
+                                        <div className="w-full mb-3">
+                                            <label
+                                            className="block uppercase text-blueGray-600  text-xs font-bold mb-2">
+                                            Old Password
+                                            </label>
+                                            <input
+                                            name="oldPassword"
+                                            onChange={handleChanges}
+                                            type="text"
+                                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-black rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="w-full lg:w-6/12 px-4">
+                                        <div className="  w-full mb-3">
+                                            <label
+                                            className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
+                                            New Password
+                                            </label>
+                                            <input
+                                            name="newPassword"
+                                            onChange={handleChanges}
+                                            type="text"
+                                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-black rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                                        </div>
+                                    </div>
+                                        
+                                    <div className="w-full lg:w-6/12 px-4">
+                                        <div className="  w-full mb-3">
+                                            <label
+                                            className="block uppercase text-blueGray-600 text-xs font-bold mb-2">
+                                            Confirm Password
+                                            </label>
+                                            <input
+                                            name="confirmPassword"
+                                            onChange={handleChanges}
+                                            type="text"
+                                            className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 md:text-black rounded text-sm   focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div style={{
+                                marginTop: 5,
+                                display: "flex",
+                                justifyContent: "center",
+                                alignContent: "center"}}>
+                                    <button className="text-lg font-semibold border-2 border-purple-800 bg-purple-700 text-white rounded-lg px-6 py-2 block shadow-xl hover:text-purple-700 hover:bg-white" >
+                                        Update
+                                    </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                
-                <div className="flex items-center justify-center">
-                    <button
-                    type="submit"
-                    className="items-center px-4 py-2 w-1/4 rounded-md text-white bg-gray-900">
-                        SAVE
-                    </button>
-                </div>
-            </form>
-        </div>
+            </div>
+        </section>
         </>
     )
 }
